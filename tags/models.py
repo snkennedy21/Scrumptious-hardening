8d97ec762b37c87d82df7408ef79fb9d1fcf4ba0
Sean Kennedy <snkennedy21@gmail.com>
2022-07-19 18:55:34 -0400

    add authors for tags

--- tags/models.py
+++ tags/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.conf import settings
+from recipes.models import USER_MODEL
+
+USER_MODEL = settings.AUTH_USER_MODEL
 
 
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=20)
     recipes = models.ManyToManyField("recipes.Recipe", related_name="tags")
+    author = models.ForeignKey(USER_MODEL, related_name='tags', on_delete=models.CASCADE, null=True)
